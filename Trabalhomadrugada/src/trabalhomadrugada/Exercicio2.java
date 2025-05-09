4e0031138941e9366d1b2fe4bda9847cfa35e9eb
ember <ember@DESKTOP-9BBRE2Q>
2025-05-09 04:38:53 -0300

    Exercicios 3,4 e uh eu n lembro mais

--- Trabalhomadrugada/src/trabalhomadrugada/Exercicio2.java
+++ Trabalhomadrugada/src/trabalhomadrugada/Exercicio2.java
@@ -8,11 +8,29 @@ public class Exercicio2 {
 		
 		Scanner teclado = new Scanner(System.in); //Cria um objeto Scanner
 		
-		System.out.println ("Sua idade:");
-		double nome = teclado.nextDouble(); //le o input
-		
-		System.out.println ("Você esta acompanhado(a) ?");
-		
+		System.out.print("Digite a idade: ");
+        int idade = teclado.nextInt();
+        
+        System.out.print("Está acompanhado? (1-sim/2-não): ");
+        int acomp = teclado.nextInt();
+        
+        System.out.print("Tem autorização especial? (1-sim/2-não): ");
+        int autori = teclado.nextInt();
+        
+        if (idade >= 18) {
+            System.out.println("Acesso permitido");
+        }
+        else if(idade >= 16) {
+        	if (acomp == 1 || autori == 1) {
+        		 System.out.println("Acesso permitido");
+        	}
+        	else {
+        		 System.out.println("Acesso negado");
+        	}
+        }
+        else {
+   		 System.out.println("Acesso negado");
+        }
 	}
 
 }
