5483486226635a4e587acc2b1b1705cf4d9fdef7
ember <ember@DESKTOP-9BBRE2Q>
2025-05-09 02:34:29 -0300

    Exercicios 1,2 e 10

---
+++ Trabalhomadrugada/src/trabalhomadrugada/Exercicio10.java
@@ -0,0 +1,34 @@
+package trabalhomadrugada;
+
+import java.util.Scanner; //Importa Scanner
+
+public class Exercicio10 {
+ 
+	public static void main (String[]Args) {
+		
+		Scanner teclado = new Scanner(System.in); //Cria um objeto Scanner
+		
+		System.out.println ("Ultimo digito da sua placa:");
+		double placa = teclado.nextDouble(); //le o input
+		String dia = ("Erro");
+		
+		if (placa==1 || placa==2) {
+			 dia = ("Segunda");
+		}
+		else if (placa==3 || placa==4) {
+			 dia = ("Terça");
+		}
+		else if (placa==5 || placa==6) {
+			 dia = ("Quarta");
+		}
+		else if (placa==7 || placa==8) {
+			 dia = ("Quinta");
+		}
+		else if (placa==9 || placa==0) {
+			 dia = ("Sexta");
+		}
+		System.out.println("Dia da semana: " +dia);
+		
+	}
+	
+}
---
+++ Trabalhomadrugada/src/trabalhomadrugada/Exercicio2.java
@@ -0,0 +1,18 @@
+package trabalhomadrugada;
+
+import java.util.Scanner; //Importa Scanner
+
+public class Exercicio2 {
+	
+	public static void main (String[]Args) {
+		
+		Scanner teclado = new Scanner(System.in); //Cria um objeto Scanner
+		
+		System.out.println ("Sua idade:");
+		double nome = teclado.nextDouble(); //le o input
+		
+		System.out.println ("Você esta acompanhado(a) ?");
+		
+	}
+
+}
